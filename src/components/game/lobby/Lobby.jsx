ee98d571c1a7394531d7c580b5c031b2ac5b8ba9
aletoledo <ale.tole.454@gmail.com>
2023-10-02 23:35:31 -0300

    Estructurar lobby
    Se estructuro el lobby

--- src/components/game/lobby/Lobby.jsx
+++ src/components/game/lobby/Lobby.jsx
@@ -1,7 +1,41 @@
-const Lobby = ({ apiData }) => {
+import FunctionButton from "../../FunctionButton/FunctionButton";
+
+
+
+const Lobby = ({ 
+    apiData
+ }) => {
+    
+    const hostName = apiData.players.filter((player) => player.table_position === 1)[0].name;
+
+
+    const data = {
+        game_id: 1,
+        player_name: hostName,
+    }
+
+        //Make the request to the server
+    const startGame = async () => {
+        try {
+        const response = await httpRequest({
+            method: "POST",
+            service: "game/start",
+            payload: data,
+        });
+        console.log("Iniciando partida");
+        return response;
+    } catch (error) {
+        console.log("Iniciando partida");
+        return error; 
+        }
+    };
+
+    
+    
     return (
         <div>
-            {JSON.stringify(apiData)}
+            <FunctionButton text={"Iniciar Partida" } onClick={startGame}/>
+            
         </div>
     )
 }
