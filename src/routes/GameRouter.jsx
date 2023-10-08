625c1edece34bd9a2ec07093c08a405aabb9211d
augustolallana <lallanaaugusto@gmail.com>
2023-10-08 18:57:51 -0300

    Implementar websockets en GameRouter

--- src/routes/GameRouter.jsx
+++ src/routes/GameRouter.jsx
@@ -1,11 +1,26 @@
+import useWebSocket from "react-use-websocket";
 import Game from "../components/game/Game"
 
+import { useState, useEffect, useContext } from "react";
+
 const GameRouter = () => {
+    const gameId = 1;
+    const { lastJsonMessage: apiData, readyState} = useWebSocket(`ws://localhost:8000/game/${gameId}`);
+    
+    useEffect(() => {
+        // 0: connecting, 1: open, 2: closing, 3: closed  
+        if (readyState === 1) {
+            setApiData(apiData.data);
+        };
+      }, [apiData]);
+
+    console.log(`apiData: ${apiData}`);
+
     return (
         <div>
-            <Game></Game>
+            <p>LOS WEB SOCKETS ESTAN TRATANDO DE ANDAR {JSON.stringify(apiData)}</p>
         </div>
-    )
-}
+    );
+};
 
 export default GameRouter;
