3d50e40b7c0cc2412ca184793d3aa2beb2dac5df
Florencia Brunello <florenciabrunello@mi.unc.edu.ar>
2023-10-29 19:22:28 -0300

    Aumentar coverage del Lobby.test

--- src/tests/Lobby.test.jsx
+++ src/tests/Lobby.test.jsx
@@ -1,18 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, test  } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, expect, test, beforeAll, afterAll, it } from "vitest";
 import { BrowserRouter } from "react-router-dom";
 import Lobby from "../components/game/lobby/Lobby";
 import { GameContext, PlayerContext } from "../components/game/Game";
+import { createServer } from "node:http";
+import { io as ioc } from "socket.io-client";
+import { Core } from '../routes/Core'
+import MockedSocket from 'socket.io-mock';
+import { userEvent } from "@testing-library/user-event";
+
 
 describe("Lobby component", () => {
-    
-    test("should render the go out game button", async () => {
-        render(<BrowserRouter><Lobby /></BrowserRouter>);
-        expect(screen.getByRole("button", { name: "Abandonar Partida" })).toBeDefined();
-    })
-        
+
     //test for players 
     test("should render the waiting for host message", async () => {
+        const socket = {
+            "state": 0,
+        };
+                
+        const playermock = {
+            "owner": false,
+        };
+
         const gameContextValue = {
         "id": 1,
         "name": "a",
@@ -43,11 +52,7 @@ describe("Lobby component", () => {
         };
 
         render(<BrowserRouter>
-                <GameContext.Provider value={gameContextValue}>
-                    <PlayerContext.Provider value={playerContextValue}>
-                        <Lobby />
-                    </PlayerContext.Provider>
-                </GameContext.Provider>
+                <Lobby socket={socket} player={playermock} gameData={{ state: 0}}></Lobby>
             </BrowserRouter>
         );
 
@@ -56,6 +61,10 @@ describe("Lobby component", () => {
 
     //test for host
     test("should render the waiting for players message", async () => {
+        const socket = {
+            "state": 0,
+        };
+        
         const gameContextValue = {
         "id": 1,
         "name": "a",
@@ -86,11 +95,7 @@ describe("Lobby component", () => {
         };
 
         render(<BrowserRouter>
-                <GameContext.Provider value={gameContextValue}>
-                    <PlayerContext.Provider value={playerContextValue}>
-                        <Lobby />
-                    </PlayerContext.Provider>
-                </GameContext.Provider>
+                <Lobby socket={socket} player={playerContextValue} gameData={{ state: 0}}></Lobby>
             </BrowserRouter>
         );
 
@@ -130,16 +135,53 @@ describe("Lobby component", () => {
         };
 
         render(<BrowserRouter>
-                <GameContext.Provider value={gameContextValue}>
-                    <PlayerContext.Provider value={playerContextValue}>
-                        <Lobby />
-                    </PlayerContext.Provider>
-                </GameContext.Provider>
+                <Lobby player={playerContextValue} gameData={{ state: 0}}></Lobby>
             </BrowserRouter>
         );
 
         const buttonElement = screen.queryByText('Iniciar Partida');
         expect(buttonElement).not.toBeNull();
     });
+
+    test("should disconnect socket when clicking Abandonar Partida button", async() => {        
+        const socketMock = new MockedSocket();
+        const utils = render(<BrowserRouter>
+            <Lobby socket={socketMock} player={{ owner: false }} gameData={{ state: 0}}></Lobby>
+        </BrowserRouter>);
+        const button = screen.getByRole("button", { name: "Abandonar Partida"});
+        await userEvent.click(button);
+        });
+});
+/*
+
+        const socketMock = new MockedSocket();
+
+        render(<BrowserRouter>
+                <Lobby socket={socketMock} player={{ owner: false }} gameData={{ state: 0}}></Lobby>
+            </BrowserRouter>
+        );
+
+        // Debugging: Output the component structure to the console to ensure the button is rendered
+        console.log(screen.debug());
+        
+})      
+/*
+        const createSocketMock = (uri, gameId, playerId) => {
+            const socketConfig = {
+            transports: ["websocket"],
+            query: {
+                "Game-Id": gameId,
+                "Player-Id": playerId
+            },
+            };
+            return socketMock;
+        }
+
+        const gameSocketMock = createSocketMock("http://localhost:8000/", gameId, playerId);  
     
-})
+
+        await goOutGameMock(data);
+
+        expect(socketMock.disconnected).toBe(true);
+        expect(navigate).toHaveBeenCalledWith("/");*/
+    
