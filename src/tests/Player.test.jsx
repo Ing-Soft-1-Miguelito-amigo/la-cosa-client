8c216f09e529f3b81a521e6393e64ff1dcd071ab
aletoledo <ale.tole.454@gmail.com>
2023-11-14 01:59:53 -0300

    subir coverage Player

--- src/tests/Player.test.jsx
+++ src/tests/Player.test.jsx
@@ -1,15 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { beforeEach, describe, expect, test } from "vitest";
 import Player from "../components/game/player/Player";
 
 const mockPlayerData = (
     name = "augusto",
+    tablePosition = 2,
     codes = ["lla", "lla", "lla", "wsk"]
 ) => ({
     "name": name,
     "owner": false,
     "id": 2,
-    "table_position": 2,
+    "table_position": tablePosition,
     "role": null,
     "alive": true,
     "quarantine": false,
@@ -64,19 +65,19 @@ const players = [
     },
     {
         "name": "player2",
-        "table_position": 1,
+        "table_position": 2,
         "alive": true,
         "quarantine": false
     },
     {
         "name": "player3",
-        "table_position": 1,
+        "table_position": 3,
         "alive": true,
         "quarantine": false
     },
     {
         "name": "player4",
-        "table_position": 1,
+        "table_position": 4,
         "alive": true,
         "quarantine": false
     }
@@ -113,18 +114,155 @@ describe("Player component", () => {
         expect(player_).toBeDefined();
     });
 
-    /*test("should call setPlayerSelected when click on player", async () => {
-        const player1 = screen.getByTestId("player-a");        
-        const player2 = screen.getByTestId("player-a2");        
-        const player3 = screen.getByTestId("player-a3");      
-    
-        fireEvent.click(player1);
-        expect(mockContext.setPlayerSelected).toHaveBeenCalledTimes(1);
-        fireEvent.click(player2);
-        expect(mockContext.setPlayerSelected).toHaveBeenCalledTimes(2);
-        fireEvent.click(player3);
-        expect(mockContext.setPlayerSelected).toHaveBeenCalledTimes(3);
-    });*/
+    test("should call setPlayerSelected when click on player", async () => {
+        const player = mockPlayerData("player1", 1);
+        const player2 = mockPlayerData("player2", 2);
+        const player3 = mockPlayerData("player3", 3);
+        const playerSelectedState = {name: "player1", setPlayerSelected: vi.fn()};
+        const cardSelected = {cardId: 1, code:"sos", number_in_card: 1, kind: 1};
+        const cardSelected2 = {cardId: 1, code:"wky", number_in_card: 1, kind: 1};
+
+        const turn_1 = turn(1, "sed", null);
+        const obstacles = [3];
+        const doorSelected = false;
+        const setDoorSelected = vi.fn();
+        render(<>
+            <Player 
+                name={"player1"}
+                playerData={players.find((p1) => p1.name === "player1")}
+                player={player}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+            <Player 
+                name={"player2"}
+                playerData={players.find((p1) => p1.name === "player2")}
+                player={player2}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+            <Player 
+                name={"player3"}
+                playerData={players.find((p1) => p1.name === "player3")}
+                player={player3}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected2}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+        </>);
+        const player_1 = screen.getByTestId("player-player1");        
+        const player_2 = screen.getByTestId("player-player2");        
+        const player_3 = screen.getByTestId("player-player3");      
+        
+        fireEvent.click(player_1);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(4);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({});
+        fireEvent.click(player_2);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(5);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({name: "player2"});
+        fireEvent.click(player_3);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(6);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({});
+    });
+
+    test("should call setPlayerSelected when click on player", async () => {
+        const player = mockPlayerData("player1", 1);
+        const player2 = mockPlayerData("player2", 2);
+        const player3 = mockPlayerData("player3", 3);
+        const player4 = mockPlayerData("player4", 4);
+        const playerSelectedState = {name: "player1", setPlayerSelected: vi.fn()};
+        const cardSelected = {cardId: 1, code:"lla", number_in_card: 1, kind: 1};
+        const cardSelected2 = {cardId: 2, code:"sed", number_in_card: 1, kind: 1};
+        const cardSelected3 = {cardId: 3, code:"sos", number_in_card: 1, kind: 1};
+        const cardSelected4 = {cardId: 3, code:"cdl", number_in_card: 1, kind: 1};
+
+        const turn_1 = turn(1, null, null);
+        const obstacles = [3];
+        const doorSelected = false;
+        const setDoorSelected = vi.fn();
+        render(<>
+            <Player 
+                name={"player1"}
+                playerData={players.find((p1) => p1.name === "player1")}
+                player={player}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+            <Player 
+                name={"player2"}
+                playerData={players.find((p1) => p1.name === "player2")}
+                player={player2}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected2}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+            <Player 
+                name={"player3"}
+                playerData={players.find((p1) => p1.name === "player3")}
+                player={player3}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected3}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+            <Player 
+                name={"player4"}
+                playerData={players.find((p1) => p1.name === "player4")}
+                player={player4}
+                playerSelectedState={playerSelectedState}
+                cardSelected={cardSelected3}
+                players={players}
+                setDiscard={vi.fn()}
+                turn={turn_1}
+                obstacles={obstacles}
+                setDoorSelected={setDoorSelected} 
+            />
+        </>);
+        const player_1 = screen.getByTestId("player-player1");        
+        const player_2 = screen.getByTestId("player-player2");        
+        const player_3 = screen.getByTestId("player-player3");      
+        const player_4 = screen.getByTestId("player-player4");      
+        
+        fireEvent.click(player_1);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(5);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({});
+        fireEvent.click(player_2);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(6);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({name: "player2"});
+        fireEvent.click(player_3);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(6);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({});
+        fireEvent.click(player_4);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledTimes(7);
+        expect(playerSelectedState.setPlayerSelected).toHaveBeenCalledWith({});
+
+    });
 
     // test("should player selected have diferent style properties than not seleted", async () => {
     //     const player1 = screen.getByTestId("player-a");        
